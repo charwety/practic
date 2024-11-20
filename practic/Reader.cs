f03b2b69d3eef8a1bb098738f8991c83b45370ba
charwety <165644160+charwety@users.noreply.github.com>
2024-11-20 09:15:18 +0300

    Add files via upload

---
+++ practic/Reader.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practic
+{
+    public class Reader
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public Reader(string id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+        public override string ToString()
+        {
+            return $"{Id}: {Name} ({Email})";
+        }
+    }
+}
